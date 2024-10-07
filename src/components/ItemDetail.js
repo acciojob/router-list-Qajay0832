1f99162094715658c667f2b86ba05d2a021155e4
Qajay0832 <qajay0832@gmail.com>
2024-10-08 03:35:00 +0530

    second

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -3,11 +3,17 @@ import { useParams } from 'react-router-dom'
 
 const ItemDetail = () => {
     let ItemId = useParams();
-
+    const items = [{ id: 1, name: 'Item 1', description: 'Description for Item 1' },
+    { id: 2, name: 'Item 2', description: 'Description for Item 2' },
+    { id: 3, name: 'Item 3', description: 'Description for Item 3' }]
+    let item=items.find(e=>e.id==ItemId.itemId);
+    
+    
+    
     return (
         <div>
-            <h2>Item {ItemId.itemId}</h2>
-            <p>Description for Item {ItemId.itemId}</p>
+            <h2>{item.name}</h2>
+            <p>{item.description}</p>
         </div>
     )
 }
